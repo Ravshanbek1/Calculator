7d0ec7050f885bf7a8bc462a5122dcb350313e63
Ravshanbek1 <you@example.com>
2019-01-20 19:51:40 -0600

    Add comment

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,10 @@
 public class Calculator {
-
+   //this is a very useful meethod.
     public static  double add(double num1, double num2){
         return num1 + num2;
     }
 
-    public static double subtract(double num1, double num2){
+    public static double subtract(double num1, double num2){git
         return num1 - num2;
    // }
 //

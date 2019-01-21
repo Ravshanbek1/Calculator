c09679ca240dd9e454a2602b29a2ba411c5a1746
Ravshanbek1 <you@example.com>
2019-01-20 19:08:31 -0600

    subtract method added

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,9 +4,9 @@ public class Calculator {
         return num1 + num2;
     }
 
-//    public static double subtract(double num1, double num2){
-//        return num1 - num2;
-//    }
+    public static double subtract(double num1, double num2){
+        return num1 - num2;
+   // }
 //
 //    public static double mulitply(double num1, double num2){
 //        return num1 * num2;

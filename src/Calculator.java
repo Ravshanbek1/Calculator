f113091505ea86e54e0cff7841544ffd4ed1a88d
Ravshanbek1 <37118150+Ravshanbek1@users.noreply.github.com>
2019-01-28 21:08:51 -0600

    14, 15 lines added

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,8 +11,8 @@ public class Calculator {
 //    public static double mulitply(double num1, double num2){
 //        return num1 * num2;
 //    }
-//    public static double divide(double num1, double num2){
-//        return num1 / num2;
+      public static double divide(double num1, double num2){
+          return num1 / num2;
 //    }
 //    public static double modules(double num1, double num2){
 //        return num1 % num2;

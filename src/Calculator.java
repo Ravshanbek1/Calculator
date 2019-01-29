2081d56dbdf9ba7f3f3cd99d192655897a714d9d
Ravshanbek1 <you@example.com>
2019-01-28 21:27:54 -0600

    LOCAL Add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,12 @@
 public class Calculator {
-   //this is a very useful meethod.
-    public static  double add(double num1, double num2){
+    //this is a very useful meethod.
+    public static double add(double num1, double num2) {
         return num1 + num2;
     }
 
-    public static double subtract(double num1, double num2){
+    public static double subtract(double num1, double num2) {
         return num1 - num2;
-   // }
+        // }
 //
 //    public static double mulitply(double num1, double num2){
 //        return num1 * num2;
@@ -17,9 +17,19 @@ public class Calculator {
 //    public static double modules(double num1, double num2){
 //        return num1 % num2;
     }
-    public static double add1(double num1, double num2){
-    return num1 + num2;
+
+    public static double add1(double num1, double num2) {
+        return num1 + num2;
     }
-    public static void multiply1(double num1, double num2){
+
+    public static void multiply1(double num1, double num2) {
+    }
+
+    public static double divide(double n1, double n2) {
+        double result = n1 / n2;
+        return result;
+
     }
 }
+
+//git clone https://github.com/SeleniumHQ/selenium.git
